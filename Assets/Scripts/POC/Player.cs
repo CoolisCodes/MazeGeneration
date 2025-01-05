0746d23b832290509ceb1a60f137b9a72c1c579f
Da-Dee <jasontobaco@gmail.com>
2025-01-05 15:54:28 +0200

    CharacterController BS

--- Assets/Scripts/POC/Player.cs
+++ Assets/Scripts/POC/Player.cs
@@ -1,3 +1,5 @@
+using JetBrains.Annotations;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -16,6 +18,7 @@ public class Player : MonoBehaviour
         if (controllerInput)
         {
             moveActionReference.action.performed += PlayerMoveController;
+            moveActionReference.action.performed += PlayerMoveKeyboard;
             moveActionReference.action.canceled += Stopped;
         }
     }
@@ -27,8 +30,37 @@ public class Player : MonoBehaviour
         characterController.Move(new Vector3(moveVec.x, 0, moveVec.y));
     }
 
+    void PlayerMoveKeyboard(InputAction.CallbackContext context)
+    { 
+        moving = true;
+        Vector2 horizontalInput = context.ReadValue<Vector2>();
+        Vector2 verticalInput = context.ReadValue<Vector2>();
+        float speed = 2.5f;
+        new Vector3(horizontalInput, 0, verticalInput);
+        
+
+    }
+
     void Stopped(InputAction.CallbackContext context)
     {
         moving = false;
     }
+    void Update()
+    {
+
+
+
+        // float based on the Axis of player's input
+        //float horizontalInput = Input.GetAxis("Horizontal");
+        //float verticalInput = Input.GetAxis("Vertical");
+        
+        //float speed = 3.5f;
+
+        //Vector3 direction = new Vector3(horizontalInput, 0 , verticalInput);
+        //transform.Translate(direction * speed * Time.deltaTime);
+    }
 }
+    
+        
+        
+
