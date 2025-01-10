5056776d38bc66dd5f940b3356250e9f98106115
Da-Dee <jasontobaco@gmail.com>
2025-01-10 14:41:56 +0200

    jump button (unimplemented)
    almost complete jump button

--- Assets/Scripts/POC/Player/Player Components/LocomotionComponent.cs
+++ Assets/Scripts/POC/Player/Player Components/LocomotionComponent.cs
@@ -9,6 +9,7 @@ public class LocomotionComponent : PlayerComponent
     public InputActionReference moveActionReference;
     public InputActionReference lookActionReference;
     public InputActionReference sprintReference;
+    public InputActionReference jumpReference;
 
     public CharacterController characterController;
 
@@ -16,17 +17,28 @@ public class LocomotionComponent : PlayerComponent
 
     public bool moving = false;
 
+    public bool isGrounded;
+
     public Vector2 vec;
     public Vector2 lookVec;
+    public Vector3 velocity;
     public bool sprinting;
+
+
 
     public float playerSpeed = 3;
     public float sprintSpeed = 10;
     public float normalSpeed = 3;
+    public float jumpHeight = 2f;
+    public float gravity = -9.81f;
 
     [SerializeField] private Transform playerCamera; // Reference to the camera
     [SerializeField] private float sensitivity = 100f; // Mouse sensitivity
     private float pitch = 0f; // Vertical rotation
+
+    public Transform groundCheck; // Reference to a ground check object
+    public float groundDistance = 0.4f; // Radius of the ground check sphere
+    public LayerMask groundMask; // Layer to identify what counts as "ground"
 
 
     public override void EnableComponent(Player player)
@@ -55,6 +67,9 @@ public class LocomotionComponent : PlayerComponent
         sprintReference.action.started += Sprint;
         sprintReference.action.canceled += Sprint;
 
+        jumpReference.action.performed += Jump;
+        jumpReference.action.canceled += Jump;
+
     }
     public override void DisableComponent()
     {
@@ -77,6 +92,8 @@ public class LocomotionComponent : PlayerComponent
 
         sprintReference.action.started -= Sprint;
         sprintReference.action.canceled -= Sprint;
+        jumpReference.action.performed -= Jump;
+        jumpReference.action.canceled -= Jump;
     }
     void PlayerMoveController(InputAction.CallbackContext context)
     {
@@ -134,13 +151,39 @@ public class LocomotionComponent : PlayerComponent
         transform.Rotate(Vector3.up * mouseX);
     }
 
+    void Jump(InputAction.CallbackContext context)
+    {
+        
+        
+        if (context.performed && isGrounded && velocity.y < 0)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        velocity.y = -2f;
+        velocity.y += gravity * Time.deltaTime;
+    }
+
+
+    void GroundCheck()
+    {
+        characterController = GetComponent<CharacterController>();
+        if (groundCheck == null)
+        {
+            Debug.LogWarning("GroundCheck transform is not assigned. Please set it in the Inspector.");
+        }
+    }
+
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
         if (!controllerInput && moving)
         {
             Vector3 move = transform.right * vec.x + transform.forward * vec.y;
             characterController.Move(move * playerSpeed * Time.deltaTime);
             //characterController.Move(new Vector3(vec.x, 0, vec.y) * speed * Time.deltaTime);
+            
         }
+        
     }
 }
